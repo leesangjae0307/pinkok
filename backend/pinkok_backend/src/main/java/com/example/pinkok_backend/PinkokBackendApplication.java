6f5250d9c38dc85dfa8a5f5dd83ebc33616865b9
JeongHun <6061pjh@gmail.com>
2026-07-25 16:44:54 +0900

    frat: backend 폴더에 스프링 부트 프로젝트 추가

---
+++ backend/pinkok_backend/src/main/java/com/example/pinkok_backend/PinkokBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pinkok_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinkokBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PinkokBackendApplication.class, args);
+    }
+
+}
